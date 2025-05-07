daaaf628109c698425cd2b661f924401dc100aa2
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2025-05-07 12:53:22 +0700

    them mot vai model cho back-end

---
+++ src/main/java/com/poly/BeeShoes/model/Notification.java
@@ -0,0 +1,26 @@
+package com.poly.BeeShoes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String createdBy;
+    private String createdTime;
+    private String title;
+    private String description;
+    private String creatorAvatarUrl;
+}
