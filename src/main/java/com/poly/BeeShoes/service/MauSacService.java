15bf679aa1a2473fb940ee88f2a6da1ee555a65f
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2025-06-14 08:59:39 +0700

    hoan thiet impl, service cua mui giay, mau sac, tags, the loai, thuong hieu

---
+++ src/main/java/com/poly/BeeShoes/service/MauSacService.java
@@ -0,0 +1,17 @@
+package com.poly.BeeShoes.service;
+
+import com.poly.BeeShoes.model.MauSac;
+
+import java.util.List;
+
+public interface MauSacService {
+    MauSac save(MauSac mauSac);
+    MauSac getById(Long id);
+    MauSac getMauSacByMa(String ma);
+    MauSac getByTen(String ten);
+    List<MauSac> getAll();
+    boolean delete(Long id);
+    boolean existsByMaMauSac(String ma);
+    List<MauSac> getAllClient();
+    boolean existsByTen(String ten,Long id);
+}
