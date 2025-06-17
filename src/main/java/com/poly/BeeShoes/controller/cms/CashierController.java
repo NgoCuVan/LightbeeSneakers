8514cf464714cd0b4e80421cf7dcb264ca05f07a
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2025-06-17 20:29:30 +0700

    fix mot vai loi trong code front-end

--- src/main/java/com/poly/BeeShoes/controller/cms/CashierController.java
+++ src/main/java/com/poly/BeeShoes/controller/cms/CashierController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/cms/cashier")
+@RequestMapping("/cms")
 @RequiredArgsConstructor
 public class CashierController {
     private final KhachHangService khachHangService;
